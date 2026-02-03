28597ec5d60a1116f9e21ca9b671e37830117b31
Emilly <165393316+emillymoitinho@users.noreply.github.com>
2026-02-03 17:54:41 -0300

    feat: adicionado seção perguntas frequentes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,13 @@ const menu = document.querySelector(".menu");
 btnMenu.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
+
+const perguntas = document.querySelectorAll(".pergunta-item");
+
+perguntas.forEach(item => {
+    const topPergunta = item.querySelector('.top-pergunta-item');
+    
+    topPergunta.addEventListener('click', () => {
+        item.classList.toggle('ativo');
+    });
+});
